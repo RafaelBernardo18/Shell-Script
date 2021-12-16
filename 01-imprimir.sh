ec52f67271c671ca68eb4949bec871068e83aa5f
RafaelBernardo18 <77741619+RafaelBernardo18@users.noreply.github.com>
2021-12-16 13:01:11 -0300

    Add files via upload

---
+++ 01-imprimir.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#primerio script om shell
+
+echo "Bem vindo ao shell scripting"
+exit 0
+
+#saia do editor de texto 
+#de a permisao de para o arquivo com chmod +x 01-imprimir.sh
+#execute com ./01-imprimir.sh
+#voce pode copiar os scripts para o diretorio /bin/ e executar assim 01-imprimir.sh
